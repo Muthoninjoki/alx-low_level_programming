b1f7334b82da4fe23625c73508d3e3c5bfc7235f
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-02 07:00:05 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ float floatType;
 long int longintType;
 long long int longlongintType;
 
-printf("Size of int is: %ld bytes\n", sizeof(integerType));
-printf("Size of char is: %ld bytes\n", sizeof(charType));
-printf("Size of float is: %ld bytes\n", sizeof(floatType));
-printf("Size of long int is: %ld bytes\n", sizeof(longintType));
-printf("Size of long long int is: %ld bytes\n", sizeof(longlongintType));
+printf("Size of char is: %ld byte(s)\n", sizeof(charType));
+printf("Size of int is: %ld byte(s)\n", sizeof(integerType));
+printf("Size of long int is: %ld byte(s)\n", sizeof(longintType));
+printf("Size of long long int is: %ld byte(s)\n", sizeof(longlongintType));
+printf("Size of float is: %ld byte(s)\n", sizeof(floatType));
 
 return (0);
 }
