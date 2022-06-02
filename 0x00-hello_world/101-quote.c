af882d9baebb4b6240661074a1383d7bf7dfa2e4
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-02 07:45:33 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+return (1);
+}
