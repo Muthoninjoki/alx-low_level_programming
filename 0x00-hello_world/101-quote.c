c7fc511380e102c6ed4bc087d3270f017c55fceb
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-02 08:27:13 -0700

    Dora

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,10 @@
 int main(void)
 {
 /**
- * main -Entry point
- * -Dora Korpar, 2015-10-19"
- *
+ * main -prints exactly "and that piece of art is useful"
+ * -Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
  * Return: Always 0 (Success)
- *
  */
 printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
