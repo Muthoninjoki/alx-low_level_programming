b1aa01725eb1ea31abd084959979b2eb6d59e184
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-02 04:36:10 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	char str[] ="Programming is like building a multilingual puzzle\n";
+	char str[] ="\"Programming is like building a multilingual puzzle\n";
 	puts(str);
     return 0;
 }
