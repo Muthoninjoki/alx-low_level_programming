1bd7f4b68070e57eefef423bcb70dad4cab51f81
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-02 13:33:39 -0700

    Dora

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@ int main(void)
 * main - Entry point
 * Return: (1)
 */
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
