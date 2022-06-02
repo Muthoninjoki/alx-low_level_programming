56d9eded4d107bc003a5204e8a1c52862d55ef42
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-02 14:10:22 -0700

    Dora

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints exactly 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'
+ * main - prints 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'
  *
  * Return: (1)
  */
-
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
