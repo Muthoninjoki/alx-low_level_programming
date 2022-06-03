524a32e955670eb0c2217ef2ad297933d7f11531
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-03 11:31:30 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,13 +6,11 @@
 int main(void)
 {
 int n;
-
+int last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+last_digit = n % 10;
 /* your code goes there */
-int last_digit;
-
-last_digit=n % 10;
 
 if (last_digit > 5)
 {
