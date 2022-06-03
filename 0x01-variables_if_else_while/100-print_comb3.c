494f5d9f307c6f5a284b8b3b7208bed3ac04787a
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-03 14:35:53 -0700

    Combo 2

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a, b;
+
+a= 48;
+b=48;
+
+while (b < 58)
+{
+a = 48;
+while (a < 58)
+{
+if (b != a && b < a)
+{
+putchar(b);
+putchar(a);
+if (a == 57 && b == 56)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+a++;
+}
+b++;
+}
+putchar('\n');
+return (0);
+}
