daf07dadf2ac3c3ecd11682ecdf69c381f281232
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-03 13:07:38 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num;
+
+for (num = '1'; num <= '10'; num++)
+{
+putchar(num);
+}
+putchar('\n');
+
+return (0);
+}
