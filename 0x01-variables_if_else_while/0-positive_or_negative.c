a3372b9a3b76d6f4dedfc9a1e8e3de2518793d8e
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-03 04:50:33 -0700

    Positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n,num[5];
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+printf("Enter Array elements : ");
+for(n=0; n<=4; n++){
+scanf("%d",&num[n]);
+}
+printf("\n\nnumber you entered are : \n");
+for(n=0; n<=4; n++){
+printf("%d, ",num[n]);
+}
+/*negative*/
+printf("\n\nNegative Number you entered are : \n");
+for(n=0; n<=4; n++){
+if(num[n]<0)
+printf("%d, ",num[n]);
+}
+/*positive*/
+printf("\n\nPositive Number you entered are : \n");
+for(n=0; n<=4; n++){
+if(num[n]>0)
+printf("%d, ",num[n]);
+}
+return (0);
+}
