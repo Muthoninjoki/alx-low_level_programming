e33d4d3eda576bceba439eca2dfe0d2260755b78
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-03 13:42:16 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int n;
+char ch;
+
+for (n = 0; n < 10; n++)
+{
+putchar(n);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+
+putchar('\n');
+
+return (0);
+}
