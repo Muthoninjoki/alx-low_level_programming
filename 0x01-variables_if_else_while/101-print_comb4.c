b408ccd4b035f13254df231e9a87ed8d8eaf5ceb
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-03 15:39:09 -0700

    Combo 3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,19 +15,20 @@ for (b = 0; b <100; b++)
 {
 if (a < b)
 {
-putchar((a / 10) + 48 && (a % 10) +48);
+putchar((a / 10) + 48);
+putchar((a % 10) +48);
 putchar(' ');
-putchar((b /10) + 48 && (b % 10) + 48);
+putchar((b /10) + 48);
+putchar(b % 10) + 48);
 
 if (a != 98 || b != 99)
 
 {
-putchar(',');
-putchar(' ');
+putchar(',', ' ');
 }
 }
 }
 }
-putchar('\n');
+
 return (0);
 }
