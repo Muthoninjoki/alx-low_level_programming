c1495ae9c88981b68c75862e8e62de2cffa7cc18
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-07 03:47:31 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,7 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void)
+void print_aplhabet(void)
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
@@ -14,5 +14,5 @@ for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
 }
 putchar('\n');
-return (0);
+
 }
