0f78d4c8c458ddd44794cb7f17013c5a014f3536
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-07 08:04:09 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,10 +4,12 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
+ * Decription: prints the alphabet in lower case 
  *
  * Return: on success 0
  * on error, -1 is returned, and error is set appropriately
  */
+
 void print_alphabet(void)
 {
 char ch;
