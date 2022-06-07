8d49b42e199e4fea2601d24c13893740020fd040
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-07 07:49:09 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,12 @@
  * Return: on success 0
  * on error, -1 is returned, and error is set appropriately
  */
-int main(void)
+void print_alphabet(void)
 {
 char ch;
 for(ch = 'a'; ch <= 'z'; ch++){
 	_putchar(ch);
 }
+_putchar('\n');
 return (0);
 }
