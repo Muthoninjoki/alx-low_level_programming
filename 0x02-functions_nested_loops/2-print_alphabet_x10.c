e38d78d421be7801f83b53446d1c2ee199be2699
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-07 10:10:42 -0700

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
+return (0);
+}
