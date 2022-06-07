488cb3f9d1d61f48dee7d440ca230ad00b741a65
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-07 03:34:35 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void);
+{
+char ch;
+for(ch = 'a'; ch <= 'z'; ch+){
+putchar(ch);
+}
+return (0);
+}
