44a02250b70078f0a9af21303ae298ef0e7fb572
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-07 13:29:54 -0700

    jack

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * jack_bauer - prints every min of the day
+ *
+ * Description: function that prints every min
+ *
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+int h;
+for (h = 0; h < 24; h++)
+{
+int m;
+for (m = 0; m < 60; m++)
+{
+_putchar('0' + (h / 10));
+_putchar('0' + (h % 10));
+_putchar(':');
+_putchar('0' + (m / 10));
+_putchar('0' + (m % 10));
+_putchar('\n');
+}
+}
