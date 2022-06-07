c3a0b9b3959a78e0e8e7917d3e9809377f59e598
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-07 15:47:43 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,7 +14,7 @@ int print_last_digit(int a)
 {
 print_last_digit(98);
 print_last_digit(0);
-n = print_last_digit(-1024);
+a = print_last_digit(-1024);
 _putchar('0' + n);
 _putchar('\n');
 return (0);
