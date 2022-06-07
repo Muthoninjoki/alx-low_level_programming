f7c4c9b9d7a5b842e721132a862513047641c537
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-07 15:04:25 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main"
+#include <unistd.h>
+
+/**
+ * add - addition
+ *
+ * Description: function that adds two integers
+ *
+ * Return: 0
+ * @a:input
+ * @b:input
+ */
+
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
