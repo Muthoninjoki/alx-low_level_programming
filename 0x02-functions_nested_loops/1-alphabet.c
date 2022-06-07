efccb700e3bbf71b34c92b1fb3acb945ee24242a
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-07 07:42:24 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: on success 0
+ * on error, -1 is returned, and error is set appropriately
+ */
+int _putchar(char c)
+{
+char ch;
+for(ch = 'a'; ch <= 'z'; ch++){
+	_putchar(ch);
+}
+return (0);
+}
