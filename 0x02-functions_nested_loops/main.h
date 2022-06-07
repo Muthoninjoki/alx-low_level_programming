07e26e54c4f1af1007053d53b6b89c8986e7a6b4
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-07 05:05:07 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,22 +1,2 @@
-#include "main.h"
+int _putchar
 
-/**
- * main - check code
- *
- * Retur: Always 0
- */
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-
-_putchar('\n');
-
-return (0);
-}
