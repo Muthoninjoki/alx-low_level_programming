fc49518d41bbd08809a3db5d7f7d5d0908a7d425
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-08 09:52:12 -0700

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,10 +14,9 @@ int i;
 int t1 = 1, t2 = 2;
 int nextTerm = t1 + t2;
 printf("Enter the number of terms: ");
-printf("Fibonacci Series: %d, %d, ", t1, t2);
 for (i = 0; i < 50; i++)
 {
-printf("%d", nextTerm);
+printf("%lu", nextTerm);
 t1 = t2;
 t2 = nextTerm;
 nextTerm = t1 + t2;
