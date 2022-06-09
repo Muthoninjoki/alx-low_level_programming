09124c17675311f60e949b4dac6eb0a32c705189
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-09 04:22:04 -0700

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * mul - check description
+ * Description: multiplies two integers
+ * @a:input
+ * @b:input
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+int mul;
+mul = a * b;
+return (mul);
+}
