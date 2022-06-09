512708d6e8bfdaf872f14a88c51cf6885aee56a8
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-09 03:57:07 -0700

    Islower

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _isupper - check description
+ * Description: checks for uppercase character
+ * @c:input
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (1);
+}
+}
