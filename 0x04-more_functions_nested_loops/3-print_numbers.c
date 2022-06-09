72d186a1b62da13771eccda225bc689d388aae89
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-09 04:33:48 -0700

    Printnumbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * printnumbers - check description
+ * print_numbers - check description
  * Description: prints the numbers from 0 to 9
  */
 
@@ -10,7 +10,7 @@ void print_numbers(void)
 
 {
 int i;
-for(i = 0; i <= 9; i++)
+for (i = 0; i <= 9; i++)
 {
 _putchar(i);
 }
