fe8b5ef24726d9517826b45d3b0645b090d083d0
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-13 14:41:17 -0700

    Puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts - check description
+ * Description: prints every other character
+ * @str:input
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+int n = 0;
+while (str[n] != '\0')
+{
+if (n % 2 == 0)
+_putchar(str[n]);
+n++;
+}
+_putchar('\n');
+}
