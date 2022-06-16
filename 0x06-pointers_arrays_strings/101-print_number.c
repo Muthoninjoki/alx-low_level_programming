d6c99cd3214fc45e7096ee02e2b3964d2271615e
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-16 14:33:32 -0700

    Print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,15 +9,20 @@
 
 void print_number(int n)
 {
-unsigned int num = n;
+unsigned int num;
 
 if (n < 0)
 {
+num = -n;
 _putchar('-');
 num = -num;
 }
+else
+{
+num = n;
+}
 
-if  ((num / 10) > 0)
+if (num / 10)
 print_number(num / 10);
 
 _putchar((num % 10) + '\0');
