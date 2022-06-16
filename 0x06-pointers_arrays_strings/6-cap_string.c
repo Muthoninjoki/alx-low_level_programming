230b2af8407806208fe38d54ce9e1080bc726a69
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-16 11:42:59 -0700

    Capitalization

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - check description
+ * Description: capitalizes all words of a string
+ * @str:input
+ * Return: 0
+ */
+
+char *cap_string(char *str)
+{
+int i = 0;
+
+while (str[++i])
+
+{
+while (!(str[i] >= 'a' && str[i] <= 'z'))
+	i++;
+
+if (str[i - 1] == ' ' ||
+	str[i] == '\t' ||
+	str[i] == '\n' ||
+	str[i] == ',' ||
+	str[i] == ';' ||
+	str[i] == '.' ||
+	str[i] == '!' ||
+	str[i] == '?' ||
+	str[i] == '"' ||
+	str[i] == '(' ||
+	str[i] == ')' ||
+	str[i] == '{' ||
+	str[i] == '}')
+str[i] -= 32;
+}
+
+return (str);
+}
