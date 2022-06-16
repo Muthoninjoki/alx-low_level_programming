cd43df755f5175df82533b66edfe4c0189b63b7c
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-16 12:00:46 -0700

    Leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - check description
+ * Description: function that encodes a string
+ * @str:string
+ * Return:0
+ */
+
+char *leet(char *str)
+{
+int i = 0, j;
+char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+while (str[++i])
+{
+for (j = 0; j <= 7; j++)
+{
+if (str[i] == leet[j] ||
+	str[i] - 32 == leet[j])
+	str[i] = j + '0';
+}
+}
+return (str);
+}
