1785a1b42ff2a4014779845f6a7b145803500239
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-16 04:15:31 -0700

    Strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int i = 0, c = 0;
+int i = 0;
 
-while (s1[c] != '\0' && s2[c] != '\0')
+while (*s1 != '\0' && *s2 != '\0')
 {
-if (s1[c] != s2[c])
+if (*s1 != *s2)
 {
 i = 1;
-break;
 }
-c++;
+s1++;
+s2++;
 }
 if (i == 0)
 return (0);
