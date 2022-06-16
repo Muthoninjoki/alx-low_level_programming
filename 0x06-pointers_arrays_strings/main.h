9a125d9efefe48cd8bbb420c8f4e96105bf253fc
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-16 14:29:41 -0700

    Print numbers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,6 +14,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+void print_number(int n);
 char *rot13(char *);
-
+int _putchar(char c);
 #endif /*MAIN_H*/
