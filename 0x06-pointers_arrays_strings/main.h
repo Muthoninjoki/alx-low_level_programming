9d4bd45768d9178713e5a389bba31aa35615758c
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-17 13:22:34 -0700

    Updates

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,4 +17,7 @@ char *leet(char *);
 void print_number(int n);
 char *rot13(char *);
 int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
 #endif /*MAIN_H*/
