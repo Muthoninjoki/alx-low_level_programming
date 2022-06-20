f79718244d05c1473f3704a29c6e7111f2227aa9
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-20 15:22:46 -0700

    Chess

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 /**
  * char prototypes()
  * int prototypes()
+ * void prototypes()
  */
 
 char *_memset(char *s, char b, unsigned int n);
@@ -12,5 +13,7 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 
 #endif /*MAIN_H*/
