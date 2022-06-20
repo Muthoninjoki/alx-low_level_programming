bd22746ffcb796d75714b498ed1ff648565cb582
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-20 15:45:34 -0700

    Diagonal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,12 +4,12 @@
  * print_diagsums - check description
  * Description: function that prints the sum of
  * the two diagonals of a square matrix of integers
- * @a:input
+ * @a: input
  * @size:size
  * Return: 0
  */
 
-void print_diagsums(int *a, int size);
+void print_diagsums(int *a, int size)
 {
 int n, i = 0, j = 0;
 for (n = 0; n < size; n++)
