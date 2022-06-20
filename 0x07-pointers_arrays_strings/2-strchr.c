528767d4527440ffb203d5ff65b0422c05ac86ee
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-20 12:23:10 -0700

    Strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,15 @@
 
 char *_strchr(char *s, char c)
 {
-while (s != '\0')
-{
-if  (s == c)
+while (*s != '\0')
 s++;
+{
+if  (*s == c)
 {
 return (s);
 }
 }
-if (s == c)
+if (*s == c)
 {
 return (s);
 }
