bd04862d7c1d4edb63362c4bdf60d00c1bc95771
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-24 10:01:43 -0700

    Mult

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check description
+ * Description: program that multiplies two numbers
+ * @argc:input
+ * @argv:input
+ * Return:0
+ */
+
+int main(int argc, char *argv[])
+{
+int i = 0;
+int n, m;
+
+if (argc == 3)
+{
+n = atoi(argv[1]);
+m = atoi(argv[2]);
+i = n * m;
+printf("%d\n", i);
+return (0);
+}
+else
+{
+print("Error\n");
+return (1);
+}
+}
