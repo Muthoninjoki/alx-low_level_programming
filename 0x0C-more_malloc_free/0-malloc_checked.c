cb876374ebf5a535238af7481d067047af346b0b
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-06-30 03:09:22 -0700

    Allocating memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* malloc_checked - check decription
+* Description: a function that allocates memorry using malloc
+* @b:integer
+* Return: a pointer to the allocated memory
+*/
+
+void *malloc_checked(unsigned int b)
+{
+void *P;
+
+p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+}
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H 
+
+/**
+* int prototypes ()
+* char prototypes ()
+*  void prototypes ()
+*/
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+#endif /*MAIN_H*/
