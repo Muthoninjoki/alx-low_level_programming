04ada7dc57ec12ccb90f911d1c0d08031971aa77
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-07-04 13:59:03 -0700

    2print fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,18 +14,14 @@ if (d == NULL)
 return;
 
 if ((*d).name == NULL)
-printf("nil\n");
-else
-printf("Name: %s\n", d->name);
-
-if ((*d).age < 0)
-printf("nil\n");
-else
-printf("Age: %f\n", d->age);
-
+{
+(*d).name = "(nil)";
+}
 if ((*d).owner == NULL)
-printf("nil\n");
-else
-printf("Owner: %s\n", d->owner);
-
+{
+(*d).owner = "(nil)";
+}
+printf("Name: %s\n", (*d).name);
+printf("Age: %f\n", (*d).age);
+printf("Owner: %s\n", (*d).owner);
 }
