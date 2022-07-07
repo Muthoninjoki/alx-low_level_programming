fa7d0fabfdccdc10911ea4926349b4d227a686a3
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-07-07 01:31:55 -0700

    Betty fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,10 @@
 #ifndef function_pointers
 #define function_pointers
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
