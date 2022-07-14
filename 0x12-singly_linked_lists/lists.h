7ec2c47b9fd9f93a47574f7a6da6e276b3de19b0
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-07-14 13:45:01 -0700

    Lists.h description addition

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,6 +10,7 @@
 * @str:string
 * @len:length
 * @next:next node
+* Description:signly linked list node structure
 */
 typedef struct list_s
 {
