cd4c60e1863517272d42890842b7784d2b70a2a7
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-07-25 08:51:21 -0700

    Reads file and prints to stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* read_textfile - check description
+* Description:reads a text file and prints it to the posix
+* @filename:pointer
+* @letters:no of letters function should read
+* Return:0
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t op, re, wr;
+char *buffer;
+
+if (filename == NULL)
+return (0);
+
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
+
+op = open(filename, O_RDONLY);
+re = read(op, buffer, letters);
+wr = write(STDOUT_FILENO, buffer, re);
+
+if (op == - 1 || re == - 1 || wr == - 1 || wr != re)
+{
+free(buffer);
+return (0);
+}
+
+free(buffer);
+close(op);
+
+return (wr);
+}
