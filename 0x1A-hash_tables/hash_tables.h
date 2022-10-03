7de81ce142cf90f1e2e28e2b20e8dc8c75c09e35
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-10-03 03:05:53 -0700

    Djb2

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -35,6 +35,7 @@ typedef struct hash_table_s
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 /**
 * struct shash_node_s - Node of a sorted hash table
