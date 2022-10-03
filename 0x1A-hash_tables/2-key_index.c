2cb6cbc26fe4986f28994a42dd2cd9a85d2518a0
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-10-03 10:27:56 -0700

    Fix

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 /**
-* Key_index - Get the index at which a key pair should be stored
-* Description: uses the djb2 algorithm
+* key_index - Get the index at which a key pair should be stored
 * @key: key
 * @size:size of array of the hash table
 * Return:index at which the key/value pair should be
 * stored in the array of the hash table
+*
+* Description:Uses the djb2 algorithm
 */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
