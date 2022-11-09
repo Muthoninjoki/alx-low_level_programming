2da829590b10d8fc1ff1aa8770f7c7024837cdb0
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-11-09 11:29:18 -0800

    Island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Describes the perimeter of the island in grid"""
+
+
+def island_perimeter(grid):
+    """"Return perimeter of the island.
+
+    0 represents a water zone and 1 represents a land zone
+    Args:
+	grid (lists)
+    Returns:
+	Perimeter of the island defined in grid
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+	for w in range(width):
+	    if grid[i][w] == 1:
+		size += 1
+		if (w > 0 and grid[i][w - 1] == 1):
+		    edges += 1
+		if (i > 0 and grid[i - 1][w] == 1):
+		    edges += 1
+    return size * 4 - edges * 2
