441482a60b350bfbed31f2d6a70769554e4e5fad
Muthoninjoki <fmuthoninjoki@gmail.com>
2022-12-30 06:33:52 -0800

    Betty check

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,7 +14,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (n == 0 && index < 64)
 		return (0);
 
-	for (i = 0; i <= 63; n >>=1, i++)
+	for (i = 0; i <= 63; n >>= 1, i++)
 	{
 		if (index == i)
 		{
